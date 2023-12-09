b17a735ecbe1a53e5baaf8286ac066d7e81e1164
Benedict Avenido <benedictavenido1320@gmail.com>
2023-12-09 23:49:45 +0800

    opitimized

--- js/index.js
+++ js/index.js
@@ -3,8 +3,6 @@ const searchBar = document.querySelector(".search-bar");
 const legend = document.getElementById("legend");
 var responseData = "";
 var searchData = "";
-
-
 var skeleton = `<div class="manga animate-flicker">
                     <img class="poster" src="assets/manga-content/image-placeholder.svg" alt="poster">
                 </div>`;
@@ -18,67 +16,9 @@ fetch("https://api.mangadex.org/manga?limit=10&includes%5B%5D=cover_art")
   .then((parsedRes) => {
     console.log(parsedRes);
     responseData = parsedRes;
-  })
-  .catch((error) => console.error(error));
-
-var loading = setInterval(() => {
-  if (responseData) {
     contentContainer.innerHTML = ``;
     for (let i = 0; i < 10; i++) {
-      let object = responseData.data;
-      let title = object[i].attributes.title.en;
-      let description = object[i].attributes.description.en;
-      let mangaId = object[i].id;
-
-      let fileName;
-      let length = object[i].relationships.length;
-      for (let j = 0; j < length; j++) {
-        if (object[i].relationships[j].type == "cover_art") {
-          fileName = object[i].relationships[j].attributes.fileName;
-        }
-      }
-      contentContainer.innerHTML += `<div class="manga">
-                                        <img class="poster" src="https://uploads.mangadex.org/covers/${mangaId}/${fileName}" alt="poster">
-                                        <div class="manga-description">
-                                            <p id="title" class="cool-text">${title}</p>
-                                            <p id="description" class="cool-text">${description}</p>
-                                        </div>
-                                      </div>
-                                      `;
-    }
-    clearInterval(loading);
-  }
-}, 1000);
-
-searchBar.addEventListener("keydown", (event) => {
-  if (event.key == "Enter") {
-
-    legend.innerHTML = `<p>Results for "${searchBar.value}"</p>`;
-
-    let input = searchBar.value.toLowerCase();
-    let encodedInput = encodeURI(input);
-
-    contentContainer.innerHTML = ``;
-
-    for (let i = 0; i < 6; i++) {
-      contentContainer.innerHTML += skeleton;
-    }
-
-    fetch(
-      `https://api.mangadex.org/manga?limit=10&title=${encodedInput}&includes%5B%5D=cover_art`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        searchData = data;
-      })
-      .catch((err) => console.log(err));
-  }
-
-   var wait = setInterval(() => {
-    if (searchData) {
-      contentContainer.innerHTML = ``;
-      for (let i = 0; i < 10; i++) {
-        let object = searchData.data;
+        let object = responseData.data;
         let title = object[i].attributes.title.en;
         let description = object[i].attributes.description.en;
         let mangaId = object[i].id;
@@ -99,7 +39,59 @@ searchBar.addEventListener("keydown", (event) => {
                                         </div>
                                         `;
       }
-      clearInterval(wait);
+  })
+  .catch((error) => console.error(error));
+
+  
+searchBar.addEventListener("keydown", (event) => {
+  if (event.key == "Enter") {
+
+    legend.innerHTML = `<p>Results for "${searchBar.value}"</p>`;
+
+    let input = searchBar.value.toLowerCase();
+    let encodedInput = encodeURI(input);
+
+    contentContainer.innerHTML = ``;
+
+    for (let i = 0; i < 6; i++) {
+      contentContainer.innerHTML += skeleton;
     }
-  }, 1000);
+
+    fetch(
+      `https://api.mangadex.org/manga?limit=10&title=${encodedInput}&includes%5B%5D=cover_art`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        searchData = data;
+        contentContainer.innerHTML = ``;
+        for (let i = 0; i < 10; i++) {
+          let object = searchData.data;
+          let title = object[i].attributes.title.en;
+          let description = object[i].attributes.description.en;
+          let mangaId = object[i].id;
+    
+          let fileName;
+          let length = object[i].relationships.length;
+          for (let j = 0; j < length; j++) {
+            if (object[i].relationships[j].type == "cover_art") {
+              fileName = object[i].relationships[j].attributes.fileName;
+            }
+          }
+          contentContainer.innerHTML += `<div class="manga">
+                                            <img class="poster" src="https://uploads.mangadex.org/covers/${mangaId}/${fileName}" alt="poster">
+                                            <div class="manga-description">
+                                                <p id="title" class="cool-text">${title}</p>
+                                                <p id="description" class="cool-text">${description}</p>
+                                            </div>
+                                          </div>
+                                          `;
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        legend.innerHTML = `<p>Results for "${searchBar.value}"</p>`;
+
+        contentContainer.innerHTML += `<h1 id="NotFound">Manga Not Found</h1>`
+      });
+  }
 });
