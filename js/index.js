725197fea18a9071d0b9090fee8e61200f347577
Benedict Avenido <benedictavenido1320@gmail.com>
2023-12-10 09:55:46 +0800

    Undefined Title and Description Bug Fixed

--- js/index.js
+++ js/index.js
@@ -18,19 +18,32 @@ fetch("https://api.mangadex.org/manga?limit=10&includes%5B%5D=cover_art")
     responseData = parsedRes;
     contentContainer.innerHTML = ``;
     for (let i = 0; i < 10; i++) {
-        let object = responseData.data;
-        let title = object[i].attributes.title.en;
-        let description = object[i].attributes.description.en;
-        let mangaId = object[i].id;
-  
-        let fileName;
-        let length = object[i].relationships.length;
-        for (let j = 0; j < length; j++) {
-          if (object[i].relationships[j].type == "cover_art") {
-            fileName = object[i].relationships[j].attributes.fileName;
-          }
+      let object = responseData.data;
+
+      let title = object[i].attributes.title.en;
+      if (title == undefined) {
+        for (let property in object[i].attributes.title) {
+          title = object[i].attributes.title[property];
         }
-        contentContainer.innerHTML += `<div class="manga">
+      }
+
+      let description = object[i].attributes.description.en;
+      if (description == undefined) {
+        for (let property in object[i].attributes.description) {
+          description = object[i].attributes.description[property];
+        }
+      }
+
+      let mangaId = object[i].id;
+
+      let fileName;
+      let length = object[i].relationships.length;
+      for (let j = 0; j < length; j++) {
+        if (object[i].relationships[j].type == "cover_art") {
+          fileName = object[i].relationships[j].attributes.fileName;
+        }
+      }
+      contentContainer.innerHTML += `<div class="manga">
                                           <img class="poster" src="https://uploads.mangadex.org/covers/${mangaId}/${fileName}" alt="poster">
                                           <div class="manga-description">
                                               <p id="title" class="cool-text">${title}</p>
@@ -38,14 +51,12 @@ fetch("https://api.mangadex.org/manga?limit=10&includes%5B%5D=cover_art")
                                           </div>
                                         </div>
                                         `;
-      }
+    }
   })
   .catch((error) => console.error(error));
 
-  
 searchBar.addEventListener("keydown", (event) => {
   if (event.key == "Enter") {
-
     legend.innerHTML = `<p>Results for "${searchBar.value}"</p>`;
 
     let input = searchBar.value.toLowerCase();
@@ -63,13 +74,27 @@ searchBar.addEventListener("keydown", (event) => {
       .then((res) => res.json())
       .then((data) => {
         searchData = data;
+        console.log(searchData);
         contentContainer.innerHTML = ``;
         for (let i = 0; i < 10; i++) {
           let object = searchData.data;
+
           let title = object[i].attributes.title.en;
+          if (title == undefined) {
+            for (let property in object[i].attributes.title) {
+              description = object[i].attributes.title[property];
+            }
+          }
+
           let description = object[i].attributes.description.en;
+          if (description == undefined) {
+            for (let property in object[i].attributes.description) {
+              description = object[i].attributes.description[property];
+            }
+          }
+
           let mangaId = object[i].id;
-    
+
           let fileName;
           let length = object[i].relationships.length;
           for (let j = 0; j < length; j++) {
@@ -91,7 +116,7 @@ searchBar.addEventListener("keydown", (event) => {
         console.log(err);
         legend.innerHTML = `<p>Results for "${searchBar.value}"</p>`;
 
-        contentContainer.innerHTML += `<h1 id="NotFound">Manga Not Found</h1>`
+        contentContainer.innerHTML += `<h1 id="NotFound">Manga Not Found</h1>`;
       });
   }
 });
